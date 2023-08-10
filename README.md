6ed092b39ff94beb77cf41207927b3ecfd448e34
fly6022 <i@fly6022.fun>
2023-08-10 11:32:05 +0800

    Update: update ``v2``.

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # OCRAPI
+
 Based on Flask &amp; CnOcr. 🎨✨可在局域网部署一个轻量级的光学字符识别系统。
 
 ## 部署
 
-只需要在程序所在目录运行以下命令，即可在```10000```端口完成部署。
+只需要在程序所在目录运行以下命令，即可在 ``10000``端口完成部署。
 
 ```bash
 py ./app.py
@@ -11,16 +12,40 @@ py ./app.py
 
 ## 使用
 
-客户端与服务端采用```POST```方法连接。
+客户端与服务端采用 ``POST``方法连接。
 
-API请求地址:```http://127.0.0.1:10000/api/ocr```
+提供了2个接口，可通过``Postman``进行测试。
+
+### 接口1
+
+API请求地址:``http://127.0.0.1:10000/api/ocr/v1``
 
 | Content-Type        | Key   | Value |
 | ------------------- | ----- | ----- |
 | multipart/form-data | image | URL   |
 
+### 接口2
+
+API请求地址:``http://127.0.0.1:10000/api/ocr/v2``
+
+| Content-Type        | Key   | Value |
+| ------------------- | ----- | ----- |
+| multipart/form-data | image | file  |
+
 ## 示例
 
-| Content-Type        | Key   | Value                                                        |
-| ------------------- | ----- | ------------------------------------------------------------ |
-| multipart/form-data | image | https://cnocr.readthedocs.io/zh/latest/examples/multi-line_cn1.png |
+### 接口1
+
+API请求地址:``http://127.0.0.1:10000/api/ocr/v1``
+
+| Content-Type        | Key   | Value |
+| ------------------- | ----- | ----- |
+| multipart/form-data | image | https://cnocr.readthedocs.io/zh/latest/examples/multi-line_cn1.png   |
+
+### 接口2
+
+API请求地址:``http://127.0.0.1:10000/api/ocr/v2``
+
+| Content-Type        | Key   | Value |
+| ------------------- | ----- | ----- |
+| multipart/form-data | image | file  |
--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # @name   : OCRAPI/app.py
 # @author : fly6022
-# @date   : 2023/7/15
+# @date   : 2023/7/28
 # @Email  : i@fly6022.fun
-# @version: 0.1.0
+# @version: 1.0.0
 # @license: BSD 3-Clause
 
 from flask import Flask, request
@@ -16,13 +16,30 @@ app = Flask(__name__);
 app.jinja_env.auto_reload = True;
 app.config['TEMPLATES_AUTO_RELOAD'] = True;
 
-@app.route('/api/ocr', methods=["POST"]) # API
-def api_get_text():
+@app.route("/")
+def index():
+    print("233!")
+    return                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          render_template("index.html")
+
+@app.route('/api/ocr/v1', methods=["POST"]) # API
+def api_get_text_v1():
     if request.method == "POST":
         image_src = request.form.get("image");
         response = req.get(image_src);
         image = Image.open(BytesIO(response.content));
-        ocr = CnOcr(det_more_configs={'use_angle_clf': True});
+        ocr = CnOcr('densenet_lite_136-gru', det_model_name='ch_PP-OCRv3_det', det_more_configs={'use_angle_clf': True});
+        outdata = ocr.ocr(image);
+        t = [];
+        for a in outdata:
+            t.append(a.get("text"));
+        text = "".join(t);
+    return text;
+
+@app.route('/api/ocr/v2', methods=["POST"]) # API
+def api_get_text_v2():
+    if request.method == "POST":
+        image = Image.open(request.files.get('image'))
+        ocr = CnOcr('densenet_lite_136-gru', det_model_name='ch_PP-OCRv3_det', det_more_configs={'use_angle_clf': True});
         outdata = ocr.ocr(image);
         t = [];
         for a in outdata:
